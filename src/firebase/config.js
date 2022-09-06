2d2d169e4c43342ff2982bb0c13ba0ba32929d64
scarfacedotcom <peteradaaku@gmail.com>
2022-09-06 08:46:58 +0100

    pages folder and components

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxeG40_AiLtDYvUC_G28XSUo5Qctz9Co4",
+  authDomain: "the-nancy-app.firebaseapp.com",
+  projectId: "the-nancy-app",
+  storageBucket: "the-nancy-app.appspot.com",
+  messagingSenderId: "1631171075",
+  appId: "1:1631171075:web:bf399868ac064759ed1436"
+};
+
+// init app
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, timestamp }
