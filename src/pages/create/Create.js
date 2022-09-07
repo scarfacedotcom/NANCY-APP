9b810c06477201adbec17581a2b0a1679b0fdfcf
scarfacedotcom <peteradaaku@gmail.com>
2022-09-07 21:46:45 +0100

    create form for project

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,10 +1,61 @@
+import { useState } from 'react'
+
 // styles
 import './Create.css'
 
 export default function Create() {
+  // form field values
+  const [name, setName] = useState('')
+  const [details, setDetails] = useState('')
+  const [dueDate, setDueDate] = useState('')
+  const [category, setCategory] = useState('')
+  const [assignedUsers, setAssignedUsers] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log(name, details, dueDate)
+  }
+
   return (
-    <div>
-      Create
+    <div className='create-form'>
+      <h2 className="page-title">Create a New Project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Project Name:</span>
+          <input 
+            required
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+        <span>Project Details:</span>
+          <textarea 
+            required
+            value={details}
+            onChange={(e) => setDetails(e.target.value)}
+          ></textarea>
+        </label>
+        <label>
+          <span>Set due date:</span>
+          <input 
+            required
+            type="date"
+            value={dueDate}
+            onChange={(e) => setDueDate(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Project Category:</span>
+        </label>
+        <label>
+          <span>Project Assigned to:</span>
+        </label>
+        <button className="btn">Add Project</button>
+
+      </form>
     </div>
   )
 }
