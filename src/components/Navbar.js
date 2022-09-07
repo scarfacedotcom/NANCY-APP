f7d34523107216e99329f680ace0b96cab1b2170
scarfacedotcom <peteradaaku@gmail.com>
2022-09-07 05:08:36 +0100

    logging users out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 import './Navbar.css'
 import Temple from '../assets/temple.svg'
 
 export default function Navbar() {
+  const { logout, isPending } = useLogout()
+
   return (
     <nav className='navbar'>
       <ul>
@@ -15,7 +18,8 @@ export default function Navbar() {
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/signup">Sign Up</Link></li>
         <li>
-          <button className="btn">Logout</button>
+          {!isPending && <button className="btn" onClick={logout}>Logout</button>}
+          {isPending && <button className="btn" disabled >Logging out ...</button>}
         </li>
       </ul>
       
