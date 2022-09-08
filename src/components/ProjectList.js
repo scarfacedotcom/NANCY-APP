f806835faf9dd2e48c22a1205eb161d16ff66656
scarfacedotcom <peteradaaku@gmail.com>
2022-09-08 22:28:29 +0100

    project grid

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,27 @@
-
+import { Link } from 'react-router-dom'
+import Avatar from './Avatar'
 // styles
 import './ProjectList.css'
 
 
 export default function ProjectList({projects}) {
   return (
-    <div>
+    <div className='project-list'>
       {projects.length === 0 && <p>Opps! No Projects Yet</p> }
       {projects.map(project => (
-        <div key={project.id}>{project.name}</div>
+        <Link to={`/projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p>Due be {project.dueDate.toDate().toDateString() }</p>
+          <div className="assigned-to">
+            <ul>
+              {project.assignedUsersList.map(user => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL} />
+                </li>
+              )) }
+            </ul>
+          </div>
+        </Link>
       ))}
       
     </div>
