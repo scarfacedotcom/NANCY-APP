e68fdd2b2f105dffdb59551e4575f4f637aa3c26
scarfacedotcom <peteradaaku@gmail.com>
2022-09-08 04:28:29 +0100

    assigned users

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,9 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Select from 'react-select'
+import { useCollection } from '../../hooks/useCollection'
 
 // styles
 import './Create.css'
 
+const categories = [
+  { value: 'development', label: 'Development' },
+  { value: 'design', label: 'Design' },
+  { value: 'sales', label: 'Sales' },
+  { value: 'marketing', label: 'Marketing' },
+]
+
 export default function Create() {
+  const { documents } = useCollection('users')
+  const [ users, setUsers ] = useState([])
+
   // form field values
   const [name, setName] = useState('')
   const [details, setDetails] = useState('')
@@ -11,10 +23,19 @@ export default function Create() {
   const [category, setCategory] = useState('')
   const [assignedUsers, setAssignedUsers] = useState([])
 
+  useEffect(()=> {
+    if(documents) {
+      const options = documents.map(user => {
+        return { value:user, label: user.displayName }
+      })
+      setUsers(options)
+    }
+  }, [documents])
+
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    console.log(name, details, dueDate)
+    console.log(name, details, dueDate, category, assignedUsers)
   }
 
   return (
@@ -49,9 +70,18 @@ export default function Create() {
         </label>
         <label>
           <span>Project Category:</span>
+          <Select
+            onChange={(option) => setCategory(option) } 
+            options={categories}
+          />
         </label>
         <label>
           <span>Project Assigned to:</span>
+          <Select
+            onChange={(option) => setAssignedUsers(option)} 
+            options={users}
+            isMulti
+          />
         </label>
         <button className="btn">Add Project</button>
 
