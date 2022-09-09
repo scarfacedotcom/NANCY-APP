5966580045d551b443965521bb26a9216f1215e3
scarfacedotcom <peteradaaku@gmail.com>
2022-09-09 19:48:27 +0100

    few changes

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -32,7 +32,7 @@ export default function ProjectComments({ project }) {
     <div className="project-comments">
       <h4>Project Comments</h4>
       
-      <ul>
+      {/* <ul>
         {project.comments.length > 0 && project.comments.map(comment => (
           <li key={comment.id} >
             <div className="comment-author">
@@ -47,6 +47,22 @@ export default function ProjectComments({ project }) {
             </div>
           </li>
         )) }
+      </ul> */}
+      <ul>
+        {project.comments.length > 0 && project.comments.map(comment => (
+          <li key={comment.id}>
+            <div className="comment-author">
+              <Avatar src={comment.photoURL} />
+              <p>{comment.displayName}</p>
+            </div>
+            <div className="comment-date">
+              <p>date here</p>
+            </div>
+            <div className="comment-content">
+              <p>{comment.content}</p>
+            </div>
+          </li>
+        ))}
       </ul>
       <form className="add-comment" onSubmit={handleSubmit}>
         <label>
