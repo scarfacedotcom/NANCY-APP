d95a3f110576faa49f2f7478e71cccefdce3ea2c
scarfacedotcom <peteradaaku@gmail.com>
2022-09-09 21:08:31 +0100

    delete projects

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -32,22 +32,6 @@ export default function ProjectComments({ project }) {
     <div className="project-comments">
       <h4>Project Comments</h4>
       
-      {/* <ul>
-        {project.comments.length > 0 && project.comments.map(comment => (
-          <li key={comment.id} >
-            <div className="comment-author">
-              <Avatar src={comment.photoURL} />
-              <p>{comment.displayName}</p>
-              <div className="comment-date">
-                <p>Date here</p>
-              </div>
-              <div className="comment-content">
-                <p>{comment.content}</p>
-              </div>
-            </div>
-          </li>
-        )) }
-      </ul> */}
       <ul>
         {project.comments.length > 0 && project.comments.map(comment => (
           <li key={comment.id}>
--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,10 +1,22 @@
 import Avatar from '../../components/Avatar'
+import { useFirestore } from "../../hooks/useFirestore"
+import { useAuthContext } from "../../hooks/useAuthContext"
+import { useHistory } from 'react-router-dom'
 
 export default function ProjectSummary({ project }) {
+  const { deleteDocument } = useFirestore('projects')
+  const { user } = useAuthContext()
+  const history = useHistory()
+
+  const handleClick = (e) => {
+    deleteDocument(project.id)
+    history.push('/')
+  }
   return (
     <div>
       <div className="project-summary">
         <h2 className="page-title">{project.name}</h2>
+        <p>By {project.createdBy.displayName}</p>
         <p className="due-date">
           Project due by {project.dueDate.toDate().toDateString()}
         </p>
@@ -20,6 +32,10 @@ export default function ProjectSummary({ project }) {
           ))}
         </div>
       </div>
+      
+      {user.uid === project.createdBy.id && (
+        <button className="btn" onClick={handleClick}>Mark as Complete</button>
+        )}
       
     </div>
   )
