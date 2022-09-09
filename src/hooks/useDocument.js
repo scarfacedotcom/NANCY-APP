6476b2a2469c21d9114336d6637c485b1cb04e9e
scarfacedotcom <peteradaaku@gmail.com>
2022-09-09 04:31:34 +0100

    use document hook

---
+++ src/hooks/useDocument.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+import { projectFirestore } from "../firebase/config"
+
+export const useDocument = (collection, id) => {
+  const [document, setDocument] = useState(null)
+  const [error, setError] = useState(null)
+
+  // real time data for a document
+  useEffect(() => {
+    const ref = projectFirestore.collection(collection).doc(id)
+
+    const unsubscribe = ref.onSnapshot((snapshot) => {
+      setDocument({ ...snapshot.data(), id: snapshot.id })
+      setError(null)
+    }, (err) => {
+      console.log(err.message)
+      setError('failed t get document')
+    })
+
+    return () = unsubscribe()
+  }, [collection, id])
+
+  return {document, error}
+}
